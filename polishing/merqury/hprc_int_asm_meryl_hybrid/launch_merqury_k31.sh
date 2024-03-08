734648ea75625da1b893958bbda666ef55f8be43
Mira <mastorasmira@gmail.com>
2024-03-08 13:06:01 -0800

    add k31 merqury

--- polishing/merqury/hprc_int_asm_meryl_hybrid/launch_merqury_k31.sh
+++ polishing/merqury/hprc_int_asm_meryl_hybrid/launch_merqury_k31.sh
@@ -6,7 +6,7 @@
 #               	sample_file.csv should have a header (otherwised first sample will be skipped)
 #					and the sample names should be in the first column
 
-#SBATCH --job-name=merqury_hybrid_hprc_int_asm_k21
+#SBATCH --job-name=merqury_hybrid_hprc_int_asm_k31
 #SBATCH --cpus-per-task=8
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
@@ -15,7 +15,7 @@
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mem=300gb
 #SBATCH --threads-per-core=1
-#SBATCH --output=merqury_hybrid_k21_submit_logs/merqury_hybrid_k21_submit_%x_%j_%A_%a.log
+#SBATCH --output=merqury_hybrid_k31_submit_logs/merqury_hybrid_k31_submit_%x_%j_%A_%a.log
 #SBATCH --time=24:00:00
 #SBATCH --array=1-2%1
 
@@ -36,11 +36,11 @@ fi
 
 echo "${sample_id}"
 
-BASE="/private/groups/patenlab/mira/hprc_polishing/hprc_int_asm/meryl_hybrid"
-RAW_WG_OUTDIR="/private/groups/patenlab/mira/hprc_polishing/hprc_int_asm/merqury_hybrid_k21/raw_wg/${sample_id}"
-POL_WG_OUTDIR="/private/groups/patenlab/mira/hprc_polishing/hprc_int_asm/merqury_hybrid_k21/pol_wg/${sample_id}"
-RAW_CONF_OUTDIR="/private/groups/patenlab/mira/hprc_polishing/hprc_int_asm/merqury_hybrid_k21/raw_conf/${sample_id}"
-POL_CONF_OUTDIR="/private/groups/patenlab/mira/hprc_polishing/hprc_int_asm/merqury_hybrid_k21/pol_conf/${sample_id}"
+BASE="/private/groups/patenlab/mira/hprc_polishing/hprc_int_asm/meryl_hybrid_k31"
+RAW_WG_OUTDIR="/private/groups/patenlab/mira/hprc_polishing/hprc_int_asm/merqury_hybrid_k31/raw_wg/${sample_id}"
+POL_WG_OUTDIR="/private/groups/patenlab/mira/hprc_polishing/hprc_int_asm/merqury_hybrid_k31/pol_wg/${sample_id}"
+RAW_CONF_OUTDIR="/private/groups/patenlab/mira/hprc_polishing/hprc_int_asm/merqury_hybrid_k31/raw_conf/${sample_id}"
+POL_CONF_OUTDIR="/private/groups/patenlab/mira/hprc_polishing/hprc_int_asm/merqury_hybrid_k31/pol_conf/${sample_id}"
 
 mkdir -p $RAW_WG_OUTDIR
 mkdir -p $POL_WG_OUTDIR
@@ -58,7 +58,7 @@ juklucas/hpp_merqury:latest merqury.sh \
 ${BASE}/${sample_id}/meryl_hybrid_outputs/${sample_id}.hybrid.meryl \
 /private/groups/hprc/assembly/batch2/${sample_id}/analysis/assembly/${sample_id}.pat.fa.gz \
 /private/groups/hprc/assembly/batch2/${sample_id}/analysis/assembly/${sample_id}.mat.fa.gz \
-${sample_id}_raw_merqury_hybrid_k21_wg
+${sample_id}_raw_merqury_hybrid_k31_wg
 
 # run hybrid merqury for polished assembly, whole genome
 time docker run --rm -u `id -u`:`id -g` \
@@ -68,7 +68,7 @@ juklucas/hpp_merqury:latest merqury.sh \
 ${BASE}/${sample_id}/meryl_hybrid_outputs/${sample_id}.hybrid.meryl \
 /private/groups/hprc/polishing/batch3/apply_GQ_filter/${sample_id}/applyPolish_pat_outputs/${sample_id}_hap1.polished.fasta \
 /private/groups/hprc/polishing/batch3/apply_GQ_filter/${sample_id}/applyPolish_mat_outputs/${sample_id}_hap2.polished.fasta \
-${sample_id}_polished_merqury_hybrid_k21_wg
+${sample_id}_polished_merqury_hybrid_k31_wg
 
 # run hybrid merqury for raw assembly, inside conf regions
 time docker run --rm -u `id -u`:`id -g` \
@@ -78,7 +78,7 @@ juklucas/hpp_merqury:latest merqury.sh \
 ${BASE}/${sample_id}/meryl_hybrid_outputs/${sample_id}.hybrid.meryl \
 /private/groups/hprc/polishing/batch3/apply_GQ_filter/hprc_polishing_QC_k31/${sample_id}/hprc_polishing_QC_outputs/Raw.hap1.insideConf.subBed.fasta \
 /private/groups/hprc/polishing/batch3/apply_GQ_filter/hprc_polishing_QC_k31/${sample_id}/hprc_polishing_QC_outputs/Raw.hap2.insideConf.subBed.fasta \
-${sample_id}_raw_merqury_hybrid_k21_conf
+${sample_id}_raw_merqury_hybrid_k31_conf
 
 # run hybrid merqury for polished assembly, inside conf regions
 
@@ -89,7 +89,7 @@ juklucas/hpp_merqury:latest merqury.sh \
 ${BASE}/${sample_id}/meryl_hybrid_outputs/${sample_id}.hybrid.meryl \
 /private/groups/hprc/polishing/batch3/apply_GQ_filter/hprc_polishing_QC_k31/${sample_id}/hprc_polishing_QC_outputs/Polished.hap1.insideConf.subBed.fasta \
 /private/groups/hprc/polishing/batch3/apply_GQ_filter/hprc_polishing_QC_k31/${sample_id}/hprc_polishing_QC_outputs/Polished.hap2.insideConf.subBed.fasta \
-${sample_id}_polished_merqury_hybrid_k21_conf
+${sample_id}_polished_merqury_hybrid_k31_conf
 
 EXITCODE=$?
 set -e
