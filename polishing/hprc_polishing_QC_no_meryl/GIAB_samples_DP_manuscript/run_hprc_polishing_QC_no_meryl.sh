1a42f3f1e991837568ac5ea1e24c6af6352f3823
Mira <mastorasmira@gmail.com>
2024-04-19 11:39:17 -0700

    submit revios

--- polishing/hprc_polishing_QC_no_meryl/GIAB_samples_DP_manuscript/run_hprc_polishing_QC_no_meryl.sh
+++ polishing/hprc_polishing_QC_no_meryl/GIAB_samples_DP_manuscript/run_hprc_polishing_QC_no_meryl.sh
@@ -46,6 +46,19 @@ sbatch \
      --sample_csv GIAB_samples_polisher_evaluation_manuscript.csv \
      --input_json_path '../hprc_polishing_QC_no_meryl_input_jsons/${SAMPLE_ID}_hprc_polishing_QC_no_meryl.json'
 
+# submit revios
+sbatch \
+     --job-name=hprc_polishing_QC_no_meryl_GIAB \
+     --array=[18,19]%2 \
+     --partition=long \
+     --mail-type=FAIL,END \
+     --mail-user=mmastora@ucsc.edu \
+     --cpus-per-task=32 \
+     --mem=400gb \
+     /private/groups/hprc/hprc_intermediate_assembly/hpc/toil_sbatch_single_machine.sh \
+     --wdl /private/groups/hprc/polishing/hpp_production_workflows/QC/wdl/workflows/hprc_polishing_QC_no_meryl.wdl \
+     --sample_csv GIAB_samples_polisher_evaluation_manuscript.csv \
+     --input_json_path '../hprc_polishing_QC_no_meryl_input_jsons/${SAMPLE_ID}_hprc_polishing_QC_no_meryl.json'
 ###############################################################################
 ##                             write output files to csv                     ##
 ###############################################################################
