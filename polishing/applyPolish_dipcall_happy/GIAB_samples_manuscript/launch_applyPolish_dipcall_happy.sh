5fbd328ce0c946f3f51c35fdeb2fc2b8329ac67d
Mira <mastorasmira@gmail.com>
2024-04-23 16:58:13 -0700

    rerun 12,14,16

--- polishing/applyPolish_dipcall_happy/GIAB_samples_manuscript/launch_applyPolish_dipcall_happy.sh
+++ polishing/applyPolish_dipcall_happy/GIAB_samples_manuscript/launch_applyPolish_dipcall_happy.sh
@@ -11,7 +11,7 @@
 #SBATCH --threads-per-core=1
 #SBATCH --output=applyPolish_dipcall_submit_logs/applyPolish_dipcall_submit_%x_%j_%A_%a.log
 #SBATCH --time=12:00:00
-#SBATCH --array=[8,9]%2
+#SBATCH --array=[12,14,16]%2
 
 set -ex
 
