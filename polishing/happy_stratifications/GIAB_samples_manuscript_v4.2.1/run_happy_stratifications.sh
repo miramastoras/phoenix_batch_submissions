10e67743ecab52dc4414550f9e742db42d99ba49
Mira <mastorasmira@gmail.com>
2024-04-25 11:59:41 -0700

    fix path

--- polishing/happy_stratifications/GIAB_samples_manuscript_v4.2.1/run_happy_stratifications.sh
+++ polishing/happy_stratifications/GIAB_samples_manuscript_v4.2.1/run_happy_stratifications.sh
@@ -5,7 +5,7 @@ cd /private/groups/patenlab/mira/hprc_polishing/polisher_evaluation/GIAB_samples
 git -C /private/groups/patenlab/mira/phoenix_batch_submissions pull
 
 ## get files to run hifiasm in sandbox...
-cp -r /private/groups/patenlab/mira/phoenix_batch_submissions/polishing/happy_stratifications/GIAB_samples_manuscript/* ./
+cp -r /private/groups/patenlab/mira/phoenix_batch_submissions/polishing/happy_stratifications/GIAB_samples_manuscript_v4.2.1/* ./
 
 #
 sbatch launch_happy.sh GIAB_samples_polisher_evaluation_manuscript.csv
