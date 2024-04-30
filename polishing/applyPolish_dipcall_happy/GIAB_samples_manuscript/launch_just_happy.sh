ceceb7fd24e4da67bf00f4cba8604f25a1870eb6
Mira <mastorasmira@gmail.com>
2024-04-30 15:43:29 -0700

    redo happy with same bed file for all

--- polishing/applyPolish_dipcall_happy/GIAB_samples_manuscript/launch_just_happy.sh
+++ polishing/applyPolish_dipcall_happy/GIAB_samples_manuscript/launch_just_happy.sh
@@ -22,7 +22,7 @@ sample_file=$1
 # Skip first row to avoid the header
 sample_id=$(awk -F ',' -v task_id=${SLURM_ARRAY_TASK_ID} 'NR>1 && NR==task_id+1 {print $1}' "${sample_file}")
 sample=$(awk -F ',' -v task_id=${SLURM_ARRAY_TASK_ID} 'NR>1 && NR==task_id+1 {print $2}' "${sample_file}")
-bed_file=$(awk -F ',' -v task_id=${SLURM_ARRAY_TASK_ID} 'NR>1 && NR==task_id+1 {print $11}' "${sample_file}")
+bed_file=$(awk -F ',' -v task_id=${SLURM_ARRAY_TASK_ID} 'NR>1 && NR==task_id+1 {print $19}' "${sample_file}")
 
 # Ensure a sample ID is obtained
 if [ -z "${sample_id}" ]; then
@@ -39,18 +39,9 @@ cd ${sample_id}
 source /private/home/mmastora/progs/miniconda3/etc/profile.d/conda.sh
 conda activate analysis
 
-GIAB_basename=`basename ${bed_file}`
-echo $GIAB_basename
-
-# intersect GIAB bed file with dipcall bed
-bedtools intersect \
-    -a ${bed_file} \
-    -b `pwd`/applyPolish_dipcall_outputs/*.dipcall.bed \
-    > `pwd`/applyPolish_dipcall_outputs/${GIAB_basename}.dipcall.bed
-
 # run happy
 bash /private/home/mmastora/progs/scripts/GIAB_happy.sh \
     `pwd`/applyPolish_dipcall_outputs/*vcf.gz \
-    `pwd`/applyPolish_dipcall_outputs/${GIAB_basename}.dipcall.bed \
+    ${bed_file} \
     `pwd`/happy_outputs/${sample_id}_happy_out \
     ${sample}
--- polishing/happy_stratifications/GIAB_samples_manuscript_v4.2.1/run_happy_stratifications.sh
+++ polishing/happy_stratifications/GIAB_samples_manuscript_v4.2.1/run_happy_stratifications.sh
@@ -17,6 +17,7 @@ cd /private/groups/patenlab/mira/hprc_polishing/polisher_evaluation/GIAB_samples
 ls | grep "HG" | while read line
     do echo $line
     cat ${line}/happy_stratifications_outputs/${line}_happy_out.extended.csv \
-    | grep "PASS" | grep -v "C1_5" | grep -v "C16_PLUS" | grep -v "C6_15" | grep -v "D1_5" \
-    | grep -v "D16_PLUS" | grep -v "D6_15" | grep -v "I1_5" | grep -v "I16_PLUS" | grep -v "I6_15"
+    | grep -v "PASS" | grep -v "C1_5" | grep -v "C16_PLUS" | grep -v "C6_15" | grep -v "D1_5" \
+    | grep -v "D16_PLUS" | grep -v "D6_15" | grep -v "I1_5" | grep -v "I16_PLUS" | grep -v "I6_15" \
+    | cut -d"," -f 1,3,8,9,11,12,13,17,24,31,38,52
   done > all_samples.stratifications.csv
