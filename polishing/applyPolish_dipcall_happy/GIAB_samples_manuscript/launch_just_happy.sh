f96a466e5cc1f338222749c4e421417f22b7d490
Mira <mastorasmira@gmail.com>
2024-04-30 19:34:08 -0700

    relaunch with new bed

--- polishing/applyPolish_dipcall_happy/GIAB_samples_manuscript/launch_just_happy.sh
+++ polishing/applyPolish_dipcall_happy/GIAB_samples_manuscript/launch_just_happy.sh
@@ -11,7 +11,7 @@
 #SBATCH --threads-per-core=1
 #SBATCH --output=applyPolish_dipcall_submit_logs/applyPolish_dipcall_submit_%x_%j_%A_%a.log
 #SBATCH --time=12:00:00
-#SBATCH --array=[2,4,6,12,14,16,20-22]%20
+#SBATCH --array=[2-22]%22
 
 set -ex
 
