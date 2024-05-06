528b7ec7779ddd1cae2e66b3a77853ab76c4e5bf
Mira <mastorasmira@gmail.com>
2024-05-06 13:01:01 -0700

    submit HG005

--- polishing/applyPolish_dipcall_happy/GIAB_samples_manuscript/launch_applyPolish_dipcall_happy.sh
+++ polishing/applyPolish_dipcall_happy/GIAB_samples_manuscript/launch_applyPolish_dipcall_happy.sh
@@ -11,7 +11,7 @@
 #SBATCH --threads-per-core=1
 #SBATCH --output=applyPolish_dipcall_submit_logs/applyPolish_dipcall_submit_%x_%j_%A_%a.log
 #SBATCH --time=12:00:00
-#SBATCH --array=[25-27]%3
+#SBATCH --array=[28]%3
 
 set -ex
 
