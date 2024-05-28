6d69cda4bbd2d8a5f34854e6ad5799567b4dd661
Mira <mastorasmira@gmail.com>
2024-05-28 10:07:46 -0400

    run DV hybrid polishing

--- polishing/applyPolish_dipcall_happy/GIAB_samples_manuscript/launch_applyPolish_dipcall_happy.sh
+++ polishing/applyPolish_dipcall_happy/GIAB_samples_manuscript/launch_applyPolish_dipcall_happy.sh
@@ -11,7 +11,7 @@
 #SBATCH --threads-per-core=1
 #SBATCH --output=applyPolish_dipcall_submit_logs/applyPolish_dipcall_submit_%x_%j_%A_%a.log
 #SBATCH --time=12:00:00
-#SBATCH --array=[32,33]%2
+#SBATCH --array=[35,36]%2
 
 set -ex
 
@@ -81,3 +81,23 @@ else
     echo "Failed."
     exit "${EXITCODE}"
 fi
+
+# chr 20
+mkdir -p happy_chr20_out
+
+# run happy
+bash /private/home/mmastora/progs/scripts/GIAB_happy_chr20.sh \
+    `pwd`/applyPolish_dipcall_outputs/*polished.dipcall.vcf.gz\
+    ${bed_file} \
+    `pwd`/happy_chr20_out/${sample_id}_happy_out \
+    ${sample}
+
+#
+mkdir -p happy_chr20_22_out
+
+# run happy
+bash /private/home/mmastora/progs/scripts/GIAB_happy_chr20_21_22.sh \
+    `pwd`/applyPolish_dipcall_outputs/*polished.dipcall.vcf.gz\
+    ${bed_file} \
+    `pwd`/happy_chr20_22_out/${sample_id}_happy_out \
+    ${sample}
