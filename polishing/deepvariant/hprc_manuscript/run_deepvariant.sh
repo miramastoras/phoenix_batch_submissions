b8e25150222858c5d5878967d9ddbf44479351ec
Mira <mastorasmira@gmail.com>
2024-05-31 15:57:07 -0700

    add input jsons

--- polishing/deepvariant/hprc_manuscript/run_deepvariant.sh
+++ polishing/deepvariant/hprc_manuscript/run_deepvariant.sh
@@ -4,12 +4,12 @@
 
 ## on personal computer...
 
-cd /Users/miramastoras/Desktop/Paten_lab/phoenix_batch_submissions/polishing/DeepPolisher/GIAB_samples_manuscript/DeepPolisher_input_jsons
+cd /Users/miramastoras/Desktop/Paten_lab/phoenix_batch_submissions/polishing/deepvariant/hprc_manuscript/deepvariant_input_jsons
 
 python3 /Users/miramastoras/Desktop/Paten_lab/hprc_intermediate_assembly/hpc/launch_from_table.py \
-     --data_table ../GIAB_samples_deepPolisher_manuscript.csv \
-     --field_mapping ../DeepPolisher_input_mapping.csv \
-     --workflow_name DeepPolisher
+     --data_table ../hprc_deepvariant.csv \
+     --field_mapping ../deepvariant_input_mapping.csv \
+     --workflow_name deepvariant
 
 ## add/commit/push to github (hprc_intermediate_assembly)
 
@@ -33,9 +33,18 @@ mkdir -p slurm_logs
 export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 
 # combine hap1 and hap2 assemblies into one file, index with bgzip
-grep -v "sample_id" hprc_deepvariant.csv | cut -f1 -d"," | while read line
-    do grep $line hprc_deepvariant.csv | cut -f23 -d","
-  done 
+grep -v "sample_id" hprc_deepvariant.csv | cut -f1 -d"," | while read line; do
+    hap1=`grep $line hprc_deepvariant.csv | cut -f19 -d","`
+    hap2=`grep $line hprc_deepvariant.csv | cut -f18 -d","`
+    cat $hap1 $hap2 > /private/groups/patenlab/mira/hprc_polishing/y2_alt_polishers/HPRC_samples_DV/assembly/${line}_diploid.fasta.gz
+    gunzip /private/groups/patenlab/mira/hprc_polishing/y2_alt_polishers/HPRC_samples_DV/assembly/${line}_diploid.fasta.gz
+    samtools faidx /private/groups/patenlab/mira/hprc_polishing/y2_alt_polishers/HPRC_samples_DV/assembly/${line}_diploid.fasta
+  done
+
+# list for csv
+grep -v "sample_id" hprc_deepvariant.csv | cut -f1 -d"," | while read line; do
+    ls /private/groups/patenlab/mira/hprc_polishing/y2_alt_polishers/HPRC_samples_DV/assembly/${line}_diploid.fasta
+  done
 
 # submit job
 sbatch \
@@ -47,18 +56,18 @@ sbatch \
      --mem=400gb \
      /private/groups/hprc/hprc_intermediate_assembly/hpc/toil_sbatch_single_machine.sh \
      --wdl /private/groups/hprc/polishing/hpp_production_workflows/QC/wdl/tasks/deepvariant.wdl \
-     --sample_csv GIAB_samples_deepPolisher_manuscript.csv \
-     --input_json_path '../DeepPolisher_input_jsons/${SAMPLE_ID}_DeepPolisher.json'
+     --sample_csv hprc_deepvariant.csv \
+     --input_json_path '../deepvariant_input_jsons/${SAMPLE_ID}_deepvariant.json'
 
 
 ###############################################################################
 ##                             write output files to csv                     ##
 ###############################################################################
 
-cd /private/groups/patenlab/mira/hprc_polishing/deepPolisher_runs/phoenix_batch_submissions_manuscript
+cd /private/groups/patenlab/mira/hprc_polishing/y2_alt_polishers/HPRC_samples_DV
 
 ## collect location of QC results
 python3 /private/groups/hprc/polishing/hprc_intermediate_assembly/hpc/update_table_with_outputs.py \
-      --input_data_table ./GIAB_samples_deepPolisher_manuscript.csv  \
-      --output_data_table ./GIAB_samples_deepPolisher_manuscript.DP.csv  \
-      --json_location '{sample_id}_DeepPolisher_outputs.json'
+      --input_data_table ./hprc_deepvariant.csv  \
+      --output_data_table ./hprc_deepvariant.updated.csv \
+      --json_location '{sample_id}_deepvariant_outputs.json'
