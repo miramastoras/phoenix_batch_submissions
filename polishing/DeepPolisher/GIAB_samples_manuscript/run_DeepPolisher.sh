34cd9adf0b93fa4da1cf240175796c98c0274492
Mira <mastorasmira@gmail.com>
2024-09-12 13:26:33 -0400

    test verkko model 2

--- polishing/DeepPolisher/GIAB_samples_manuscript/run_DeepPolisher.sh
+++ polishing/DeepPolisher/GIAB_samples_manuscript/run_DeepPolisher.sh
@@ -35,7 +35,7 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 # submit job
 sbatch \
      --job-name=DeepPolisher-manuscript \
-     --array=[13]%1 \
+     --array=[15,16]%2 \
      --partition=long \
      --cpus-per-task=32 \
      --mail-type=FAIL,END \
