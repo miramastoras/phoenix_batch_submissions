bbeaa061886dbcb496c8eb7de36e0374df7ab10e
Mira <mastorasmira@gmail.com>
2024-09-17 19:10:04 -0700

    run verkko model2

--- polishing/hprc_polishing_QC_no_meryl/GIAB_samples_DP_manuscript_k31/run_hprc_polishing_QC_no_meryl.sh
+++ polishing/hprc_polishing_QC_no_meryl/GIAB_samples_DP_manuscript_k31/run_hprc_polishing_QC_no_meryl.sh
@@ -35,7 +35,7 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 # submit all
 sbatch \
      --job-name=hprc_polishing_QC_no_meryl_GIAB \
-     --array=[48,49]%2 \
+     --array=[50,51,52,53]%4 \
      --exclude=phoenix-[09,10,22,23,24,18] \
      --partition=long \
      --mail-type=FAIL,END \
