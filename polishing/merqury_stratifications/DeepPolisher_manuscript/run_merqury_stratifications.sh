2a52d58c3b2f82aeae58649ac68c13c7bd4fa0a6
Mira <mastorasmira@gmail.com>
2024-10-03 14:40:59 -0700

    fix incorrect filepaths

--- polishing/merqury_stratifications/DeepPolisher_manuscript/run_merqury_stratifications.sh
+++ polishing/merqury_stratifications/DeepPolisher_manuscript/run_merqury_stratifications.sh
@@ -37,8 +37,9 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 # submit non-trio samples
 sbatch \
      --job-name=merqury_stratifications \
-     --array=[1-22]%22 \
-     --partition=long \
+     --array=[1]%1 \
+     --partition=medium \
+     --time=12:00:00 \
      --exclude=phoenix-[09,10,22,23,24,18] \
      --cpus-per-task=32 \
      --mem=400gb \
