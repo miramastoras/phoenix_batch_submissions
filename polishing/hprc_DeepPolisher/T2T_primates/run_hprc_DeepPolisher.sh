47c0878723d3b1e989a653bd132f5a1cf3cddeb4
Mira <mastorasmira@gmail.com>
2024-10-04 12:17:43 -0700

    polish baguette hifiasm

--- polishing/hprc_DeepPolisher/T2T_primates/run_hprc_DeepPolisher.sh
+++ polishing/hprc_DeepPolisher/T2T_primates/run_hprc_DeepPolisher.sh
@@ -28,7 +28,6 @@ git -C /private/groups/patenlab/mira/phoenix_batch_submissions pull
 ## check that github repo is up to date
 git -C /private/groups/hprc/polishing/hpp_production_workflows/ pull
 
-## get files to run hifiasm in sandbox...
 cp -r /private/groups/patenlab/mira/phoenix_batch_submissions/polishing/hprc_DeepPolisher/T2T_primates/* ./
 
 mkdir -p slurm_logs
@@ -36,11 +35,11 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 
 # submit job
 sbatch \
-     --job-name=T2T_primates_hprc-DeepPolisher \
-     --array=[1-6]%6 \
+     --job-name=T2T_primates_hprc-DeepPolisher_marmoset \
+     --array=[7]%1 \
      --partition=long \
      --cpus-per-task=32 \
-     --exclude=phoenix-[09,10,22,23,24] \
+     --exclude=phoenix-[09,10,22,23,24,18] \
      --mail-type=FAIL,END \
      --mail-user=mmastora@ucsc.edu \
      --mem=400gb \
@@ -55,7 +54,7 @@ sbatch \
 
 cd /private/groups/patenlab/mira/t2t_primates_polishing/hprc_DeepPolisher
 
-## collect location of QC results
+## collect location of results
 python3 /private/groups/hprc/hprc_intermediate_assembly/hpc/update_table_with_outputs.py \
       --input_data_table T2T_primates_all_manuscript.csv  \
       --output_data_table T2T_primates_all_manuscript.polished.csv  \
