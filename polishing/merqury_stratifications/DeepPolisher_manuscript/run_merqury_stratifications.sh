2d9d6544d775094695af564470bc2d0a866b4b66
Mira <mastorasmira@gmail.com>
2024-10-08 18:03:58 -0700

    run giab conf

--- polishing/merqury_stratifications/DeepPolisher_manuscript/run_merqury_stratifications.sh
+++ polishing/merqury_stratifications/DeepPolisher_manuscript/run_merqury_stratifications.sh
@@ -37,8 +37,8 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 # submit non-trio samples
 sbatch \
      --job-name=merqury_stratifications \
-     --array=[35-39,46-50]%12 \
-     --partition=high_priority \
+     --array=[51-55]%12 \
+     --partition=medium \
      --time=12:00:00 \
      --exclude=phoenix-[09,10,22,23,24,18] \
      --cpus-per-task=32 \
