37c894a80768457a924c4fbf6b48a9db8234bbaf
Mira <mastorasmira@gmail.com>
2024-10-11 12:45:29 -0700

    run

--- polishing/merqury_GIAB_callable_polished/DP_manuscript/run_merqury_GIAB_callable_polished.sh
+++ polishing/merqury_GIAB_callable_polished/DP_manuscript/run_merqury_GIAB_callable_polished.sh
@@ -26,13 +26,16 @@ cd /private/groups/patenlab/mira/hprc_polishing/polisher_evaluation/Merqury_GIAB
 ## check that github repo is up to date
 git -C /private/groups/patenlab/mira/phoenix_batch_submissions pull
 
-## get files to run hifiasm in sandbox...
+## check that github repo is up to date
+git -C /private/home/mmastora/progs/hpp_production_workflows/QC/wdl/ pull
+
+## get files to run in sandbox...
 cp -r /private/groups/patenlab/mira/phoenix_batch_submissions/polishing/merqury_GIAB_callable_polished/DP_manuscript/* ./
 
 mkdir -p slurm_logs
 export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 
-# submit non-trio samples
+# submit samples
 sbatch \
      --job-name=merqury_stratifications \
      --array=[1,4-7,9-17]%20 \
@@ -47,3 +50,21 @@ sbatch \
      --wdl /private/home/mmastora/progs/hpp_production_workflows/QC/wdl/workflows/merqury_GIAB_callable_polished.wdl \
      --sample_csv Merqury_GIAB_callable_polished.csv \
      --input_json_path '../merqury_GIAB_callable_polished_input_jsons/${SAMPLE_ID}_merqury_GIAB_callable_polished.json'
+
+
+######################### Get outputs ##########################
+
+cd /private/groups/patenlab/mira/hprc_polishing/polisher_evaluation/Merqury_GIAB_callable_polished/DP_manuscript/
+
+## collect location of QC results
+python3 /private/groups/hprc/polishing/hprc_intermediate_assembly/hpc/update_table_with_outputs.py \
+      --input_data_table Merqury_GIAB_callable_polished.csv  \
+      --output_data_table Merqury_GIAB_callable_polished.results.csv  \
+      --json_location '{sample_id}_merqury_GIAB_callable_polished_outputs.json'
+
+
+cut -f1 -d"," Merqury_GIAB_callable_polished.csv | grep -v "sample_id" | while read line; do
+  inside=`cat $line/analysis/merqury_GIAB_callable_polished_outputs/${line}.insideBed.subBed.merqury.qv | cut -f4 | tail -n 1`
+  outside=`cat $line/analysis/merqury_GIAB_callable_polished_outputs/${line}.outsideBed.subBed.merqury.qv | cut -f4 | tail -n 1`
+  echo ${line},${inside},${outside}
+done > all_results.csv
