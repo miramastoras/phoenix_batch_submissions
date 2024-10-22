02cec70f9af4f80a9189e5e1b3e5f389af850adc
Mira <mastorasmira@gmail.com>
2024-10-22 10:23:44 -0700

    rerun HG005

--- polishing/merqury_GIAB_callable_polished/DP_manuscript/run_merqury_GIAB_callable_polished.sh
+++ polishing/merqury_GIAB_callable_polished/DP_manuscript/run_merqury_GIAB_callable_polished.sh
@@ -38,7 +38,7 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 # submit samples
 sbatch \
      --job-name=merqury_stratifications \
-     --array=[28,29]%2 \
+     --array=[30,31]%2 \
      --partition=high_priority \
      --time=12:00:00 \
      --exclude=phoenix-[09,10,22,23,24,18] \
