b267e1701f3c6991d58c93e87a434951a1179aa3
Mira <mastorasmira@gmail.com>
2024-10-25 11:47:40 -0700

    fix winnowmap options

--- workflows/long_read_aligner_scattered/run_long_read_aligner_scattered.sh
+++ workflows/long_read_aligner_scattered/run_long_read_aligner_scattered.sh
@@ -39,9 +39,9 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 # submit job
 sbatch \
      --job-name=long_read_aligner_scattered \
-     --array=[1-4]%4 \
-     --partition=medium \
-     --time=12:00:00 \
+     --array=[4]%1 \
+     --partition=long \
+     --time=24:00:00 \
      --cpus-per-task=32 \
      --exclude=phoenix-[09,10,22,23,24,18] \
      --mem=400gb \
@@ -51,3 +51,15 @@ sbatch \
      --wdl ~/progs/flagger/wdls/workflows/long_read_aligner_scattered.wdl \
      --sample_csv long_read_aligner_scattered.csv \
      --input_json_path '../long_read_aligner_scattered_input_jsons/${SAMPLE_ID}_long_read_aligner_scattered.json'
+
+###############################################################################
+##                             write output files                   ##
+###############################################################################
+
+cd /private/groups/patenlab/mira/phoenix_batch_executions/workflows/long_read_aligner_scattered
+
+
+python3 /private/groups/hprc/polishing/hprc_intermediate_assembly/hpc/update_table_with_outputs.py \
+      --input_data_table long_read_aligner_scattered.csv  \
+      --output_data_table long_read_aligner_scattered.outputs.csv  \
+      --json_location '{sample_id}_long_read_aligner_scattered_outputs.json'
