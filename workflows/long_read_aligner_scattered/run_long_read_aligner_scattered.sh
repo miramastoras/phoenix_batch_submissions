50e18e5128f1ac9e85d07ca84df280c8e7b9f721
Mira <mastorasmira@gmail.com>
2024-10-26 11:49:50 -0700

    fix winnowmap runs

--- workflows/long_read_aligner_scattered/run_long_read_aligner_scattered.sh
+++ workflows/long_read_aligner_scattered/run_long_read_aligner_scattered.sh
@@ -39,9 +39,9 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 # submit job
 sbatch \
      --job-name=long_read_aligner_scattered \
-     --array=[4]%1 \
-     --partition=long \
-     --time=24:00:00 \
+     --array=[1-2]%2 \
+     --partition=medium \
+     --time=12:00:00 \
      --cpus-per-task=32 \
      --exclude=phoenix-[09,10,22,23,24,18] \
      --mem=400gb \
