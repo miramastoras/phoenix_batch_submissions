55c08be7761b1a36b4e98b8f95592847380eae77
Mira <mastorasmira@gmail.com>
2024-10-29 12:53:25 -0700

    submit KOLF QC

---
+++ workflows/hprc_polishing_QC/run_hprc_polishing_QC.sh
@@ -0,0 +1,53 @@
+###############################################################################
+##                             create input jsons                            ##
+###############################################################################
+## workflow name = hprc_polishing_QC
+
+## on personal computer...
+
+# Remove top up data from data table
+
+mkdir -p ~/Desktop/github_repos/phoenix_batch_submissions/workflows/hprc_polishing_QC/hprc_polishing_QC_input_jsons
+cd ~/Desktop/github_repos/phoenix_batch_submissions/workflows/hprc_polishing_QC/hprc_polishing_QC_input_jsons
+
+python3 /Users/miramastoras/Desktop/Paten_lab/hprc_intermediate_assembly/hpc/launch_from_table.py \
+     --data_table ../hprc_polishing_QC.csv \
+     --field_mapping ../hprc_polishing_QC_input_mapping.csv \
+     --workflow_name hprc_polishing_QC
+
+## add/commit/push to github (hprc_intermediate_assembly)
+
+###############################################################################
+##                             create launch workflow                      ##
+###############################################################################
+
+## on HPC...
+
+## check that github repo is up to date
+git -C  /private/groups/patenlab/mira/phoenix_batch_submissions pull
+
+# move to working dir
+mkdir -p /private/groups/patenlab/mira/phoenix_batch_executions/workflows/hprc_polishing_QC
+cd /private/groups/patenlab/mira/phoenix_batch_executions/workflows/hprc_polishing_QC
+
+## get files
+cp -r /private/groups/patenlab/mira/phoenix_batch_submissions/workflows/hprc_polishing_QC/* ./
+
+mkdir -p slurm_logs
+export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
+
+# submit job
+sbatch \
+     --job-name=hprc_polishing_QC \
+     --array=[1]%1 \
+     --partition=medium \
+     --time=12:00:00 \
+     --cpus-per-task=32 \
+     --exclude=phoenix-[09,10,22,23,24,18] \
+     --mem=400gb \
+     --mail-type=FAIL,END \
+     --mail-user=mmastora@ucsc.edu \
+     /private/groups/hprc/hprc_intermediate_assembly/hpc/toil_sbatch_single_machine.sh \
+     --wdl ~/progs/hpp_production_workflows/QC/wdl/workflows/hprc_polishing_QC.wdl \
+     --sample_csv hprc_polishing_QC.csv \
+     --input_json_path '../hprc_polishing_QC_input_jsons/${SAMPLE_ID}_hprc_polishing_QC.json'
