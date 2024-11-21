865b7430f8317205704ad3b3a776990f03e132ff
Mira <mastorasmira@gmail.com>
2024-11-20 16:56:04 -0800

    polish element assemblies

--- workflows/applyPolish/run_applyPolish.sh
+++ workflows/applyPolish/run_applyPolish.sh
@@ -42,9 +42,9 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 # submit job
 sbatch \
      --job-name=applyPolish \
-     --array=[1-4]%4 \
-     --partition=high_priority \
-     --time=12:00:00 \
+     --array=[5-6]%4 \
+     --partition=short \
+     --time=1:00:00 \
      --cpus-per-task=32 \
      --exclude=phoenix-[09,10,22,23,24,18] \
      --mem=400gb \
