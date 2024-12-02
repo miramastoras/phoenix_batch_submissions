5367eaae970ac933f9484057246280738bc70a2f
Mira <mastorasmira@gmail.com>
2024-12-02 11:24:43 -0800

    run maisie hic hifiasm qc

--- workflows/hprc_polishing_QC/run_hprc_polishing_QC.sh
+++ workflows/hprc_polishing_QC/run_hprc_polishing_QC.sh
@@ -39,7 +39,7 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 # submit job
 sbatch \
      --job-name=hprc_polishing_QC \
-     --array=[1]%1 \
+     --array=[5]%1 \
      --partition=medium \
      --time=12:00:00 \
      --cpus-per-task=32 \
