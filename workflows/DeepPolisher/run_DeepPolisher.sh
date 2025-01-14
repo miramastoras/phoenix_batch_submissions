f8a93a8eb195874c66df70d71d610ffa17811080
Mira <mastorasmira@gmail.com>
2025-01-14 10:57:24 -0800

    update

--- workflows/DeepPolisher/run_DeepPolisher.sh
+++ workflows/DeepPolisher/run_DeepPolisher.sh
@@ -39,7 +39,7 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 # submit job
 sbatch \
      --job-name=DeepPolisher \
-     --array=[4]%1 \
+     --array=[5]%1 \
      --partition=long \
      --time=72:00:00 \
      --cpus-per-task=32 \
