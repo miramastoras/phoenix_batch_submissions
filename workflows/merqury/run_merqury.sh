25969fe00282681bf61bdba4de85d381205dba49
Mira <mastorasmira@gmail.com>
2025-02-03 12:28:27 -0800

    GQ3

--- workflows/merqury/run_merqury.sh
+++ workflows/merqury/run_merqury.sh
@@ -39,7 +39,7 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 # submit job
 sbatch \
      --job-name=merqury \
-     --array=[17,18]%4 \
+     --array=[20]%4 \
      --partition=medium \
      --time=12:00:00 \
      --cpus-per-task=32 \
