c690ff8df45cef47da7d3b718b2e3d779ebafdfc
Mira <mastorasmira@gmail.com>
2025-02-08 15:11:10 -0800

    run merqury on verkko wg

--- workflows/hprc_polishing_QC_no_meryl/hprc_polishing_QC_no_meryl.sh
+++ workflows/hprc_polishing_QC_no_meryl/hprc_polishing_QC_no_meryl.sh
@@ -39,7 +39,7 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 # submit job
 sbatch \
      --job-name=hprc_polishing_QC_no_meryl \
-     --array=[3]%2 \
+     --array=[4]%2 \
      --partition=medium \
      --time=12:00:00 \
      --cpus-per-task=32 \
