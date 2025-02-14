bea54b96679f440efc2250b7147734b964de00af
Mira <mastorasmira@gmail.com>
2025-02-13 18:08:45 -0800

    run dv on bwa bams

--- workflows/deepvariant/deepvariant.sh
+++ workflows/deepvariant/deepvariant.sh
@@ -39,7 +39,7 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 # submit job
 sbatch \
      --job-name=deepvariant \
-     --array=[12-15]%4 \
+     --array=[16-17]%6 \
      --partition=medium \
      --time=12:00:00 \
      --cpus-per-task=32 \
