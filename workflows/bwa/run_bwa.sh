9d8b9a841f3cf9350425f6f9bdbb3013ee3a6da7
Mira <mastorasmira@gmail.com>
2025-03-24 14:46:18 -0700

    run element to HG002 diploid

--- workflows/bwa/run_bwa.sh
+++ workflows/bwa/run_bwa.sh
@@ -39,7 +39,7 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 # submit job
 sbatch \
      --job-name=bwa \
-     --array=[7-12]%6 \
+     --array=[13]%1 \
      --partition=long \
      --time=72:00:00 \
      --cpus-per-task=32 \
