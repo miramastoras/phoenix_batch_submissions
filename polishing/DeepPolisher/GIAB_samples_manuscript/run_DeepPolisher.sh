80f8d3d8d0b32b8cd91087606e1a5c85eee1beca
Mira <mastorasmira@gmail.com>
2025-09-25 16:59:42 -0700

    test

--- polishing/DeepPolisher/GIAB_samples_manuscript/run_DeepPolisher.sh
+++ polishing/DeepPolisher/GIAB_samples_manuscript/run_DeepPolisher.sh
@@ -58,3 +58,5 @@ python3 /private/groups/hprc/polishing/hprc_intermediate_assembly/hpc/update_tab
       --input_data_table ./GIAB_samples_deepPolisher_manuscript.csv  \
       --output_data_table ./GIAB_samples_deepPolisher_manuscript.DP.csv  \
       --json_location '{sample_id}_DeepPolisher_outputs.json'
+
+# test
