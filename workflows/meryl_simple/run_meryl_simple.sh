b4e72600193039706793a77992e3081cefbd2957
Mira <mastorasmira@gmail.com>
2025-09-26 13:10:15 -0700

    test meryl wdl

---
+++ workflows/meryl_simple/run_meryl_simple.sh
@@ -0,0 +1,53 @@
+###############################################################################
+##                             create input jsons                            ##
+###############################################################################
+## workflow name = meryl_simple
+
+## on personal computer...
+
+# Remove top up data from data table
+
+mkdir -p ~/Desktop/github_repos/phoenix_batch_submissions/workflows/meryl_simple/meryl_simple_input_jsons
+cd ~/Desktop/github_repos/phoenix_batch_submissions/workflows/meryl_simple/meryl_simple_input_jsons
+
+python3 /Users/miramastoras/Desktop/Paten_lab/hprc_intermediate_assembly/hpc/launch_from_table.py \
+     --data_table ../meryl_simple.csv \
+     --field_mapping ../meryl_simple_input_mapping.csv \
+     --workflow_name meryl_simple
+
+## add/commit/push to github (hprc_intermediate_assembly)
+
+###############################################################################
+##                             create launch workflow                      ##
+###################''############################################################
+
+## on HPC...
+
+## check that github repo is up to date
+git -C  /private/groups/patenlab/mira/phoenix_batch_submissions pull
+
+# move to working dir
+mkdir -p /private/groups/patenlab/mira/phoenix_batch_executions/workflows/meryl_simple
+cd /private/groups/patenlab/mira/phoenix_batch_executions/workflows/meryl_simple
+
+## get files
+cp -r /private/groups/patenlab/mira/phoenix_batch_submissions/workflows/meryl_simple/* ./
+
+mkdir -p slurm_logs
+export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
+
+# submit job
+sbatch \
+     --job-name=meryl_simple \
+     --array=[1]%1 \
+     --partition=medium \
+     --time=12:00:00 \
+     --cpus-per-task=32 \
+     --exclude=phoenix-[09,10,22,23,24,18] \
+     --mem=200gb \
+     --mail-type=FAIL,END \
+     --mail-user=mmastora@ucsc.edu \
+     /private/groups/hprc/hprc_intermediate_assembly/hpc/toil_sbatch_single_machine.sh \
+     --wdl ~/progs/hpp_production_workflows/QC/wdl/tasks/meryl_simple.wdl \
+     --sample_csv meryl_simple.csv \
+     --input_json_path '../meryl_simple_input_jsons/${SAMPLE_ID}_meryl_simple.json'
